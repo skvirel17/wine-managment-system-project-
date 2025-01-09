7b2b43824fc6a0a8b9dc425cc9488dadc677c2e4
skvirel17 <averman1998@gmail.com>
2025-01-09 17:36:09 +0200

    Initial commit

---
+++ src/entity/Manifacture.java
@@ -0,0 +1,11 @@
+package entity;
+
+public class Manifacture {
+    public static String manifactureNimber;
+    public static String fullName;
+    public static int contactPhoneNumber;
+    public static String addressManifacture;
+    public static String email;
+
+
+}
