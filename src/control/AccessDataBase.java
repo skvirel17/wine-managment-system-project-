db24336be13dd0a65e8a5de71d78e3d1987052d0
skvirel17 <averman1998@gmail.com>
2025-01-10 20:18:07 +0200

    start point ucanAccess is work

--- src/control/AccessDataBase.java
+++ src/control/AccessDataBase.java
@@ -5,44 +5,41 @@ import entity.Consts;
 import javax.management.Query;
 import java.sql.*;
 
-public class AccessDataBase {
-//    public static void main(String[] args) { /Users/maksimaverman/Desktop/proga/HW2_315063081_337759070/src/entity/database.accdb
-//        query("select * FROM TblWines");
-//
-//    }
+import static entity.Consts.CONN_STR;
 
+public class AccessDataBase {
 
     public static void main(String[] args) {
         try {
+            System.setProperty("ucanaccess.disable.autoLoadFunctions", "true");
             Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-            Connection connection = DriverManager.getConnection("jdbc:ucanaccess://Users/maksimaverman/Desktop/proga/HW2_315063081_337759070/src/entity/database.accdb");
+            Connection connection = DriverManager.getConnection("jdbc:ucanaccess://C:/database.accdb");
             System.out.println("Connection established successfully!");
+            query("select * FROM TblWines", connection);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
 
 
+    }
+
+    public static void query(String query, Connection connection) {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("wineCatalogNumber");
+                String name = resultSet.getString("wineName");
+                System.out.println("id = " + id + "," + " name = " + name);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
-//    public static void query(String query){
-//        try {
-//            Connection connection = DriverManager.getConnection("jdbc:ucanaccess://src/entity/database.accdb");
-//
-//            Statement statement = connection.createStatement();
-//            ResultSet resultSet = statement.executeQuery(query);
-//
-//            while(resultSet.next()){
-//                int id = resultSet.getInt("wineCatalogNumber");
-//                String name = resultSet.getString("wineName");
-//                System.out.println("id = " + id + "," + " name = " + name);
-//            }
-//
-//
-//        } catch (SQLException e){
-//            e.printStackTrace();
-//        }
 
-
-//    }
-//}
